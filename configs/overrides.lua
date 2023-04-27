6b66345e0e7256ef5077e79b1482dafdf3499674
wuyi <wuyi@xiaoduotech.com>
2023-04-27 09:28:05 +0800

    update

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -120,37 +120,4 @@ M.cmp = function ()
   }
 end
 
--- M.statusline = {
---       separator_style = "round",
---       overriden_modules = function()
---           local st_modules = require "nvchad_ui.statusline.modules"
---           local fn = vim.fn
---           local sep_style = vim.g.statusline_sep_style
---           local separators = (type(sep_style) == "table" and sep_style)
---           or require("nvchad_ui.icons").statusline_separators[sep_style]
---           local sep_r = separators["right"]
---
---           st_modules.fileInfo = function()
---               local icon = "  "
---               local filename = (fn.expand "%" == "" and "Empty ") or fn.expand "%:."
---
---               if filename ~= "Empty " then
---                   local devicons_present, devicons = pcall(require, "nvim-web-devicons")
---
---                   if devicons_present then
---                       local ft_icon = devicons.get_icon(filename)
---                       icon = (ft_icon ~= nil and " " .. ft_icon) or ""
---                   end
---
---                   filename = " " .. filename .. " "
---               end
---
---               return "%#St_file_info#" .. icon .. filename .. "%#St_file_sep#" .. sep_r
---           end;
---
---
---           return st_modules;
---       end
---  }
-
 return M
